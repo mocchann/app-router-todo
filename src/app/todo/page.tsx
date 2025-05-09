75d43aad249d95943dd232940572388dad217110
もっちゃん <s7056i3056@gmail.com>
2025-05-09 23:00:00 +0900

    feat: todo index

---
+++ src/app/todo/page.tsx
@@ -0,0 +1,20 @@
+const TodoIndex = () => {
+  return (
+    <>
+      <h2>todo index</h2>
+      <ul>
+        <li>
+          todo1<button>edit</button>
+        </li>
+        <li>
+          todo2<button>edit</button>
+        </li>
+        <li>
+          todo2<button>edit</button>
+        </li>
+      </ul>
+    </>
+  );
+};
+
+export default TodoIndex;
