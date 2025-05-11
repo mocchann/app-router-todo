6d6c2efe1f4946361b72c285cf3efe91f7eb3799
もっちゃん <s7056i3056@gmail.com>
2025-05-11 23:00:00 +0900

    feat: add delete

--- src/app/todo/[todoId]/edit/page.tsx
+++ src/app/todo/[todoId]/edit/page.tsx
@@ -1,8 +1,13 @@
 const TodoEdit = () => {
   return (
-    <form action="/todo/1/edit">
-      <input type="text" />
-    </form>
+    <>
+      <form action="/todo/1/edit" method="post">
+        <input type="text" />
+      </form>
+      <form action="/todo/1/delete" method="post">
+        <button>delete</button>
+      </form>
+    </>
   );
 };
 
