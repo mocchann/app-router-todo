80775f7dcec469f39c3fdf92a23a002027fd0440
もっちゃん <s7056i3056@gmail.com>
2025-05-14 23:00:00 +0900

    feat: todo create

---
+++ src/app/todo/create/page.tsx
@@ -0,0 +1,12 @@
+const TodoCreate = () => {
+  return (
+    <>
+      <h2>todo create</h2>
+      <form action="/todo" method="post">
+        <input type="text" />
+      </form>
+    </>
+  );
+};
+
+export default TodoCreate;
