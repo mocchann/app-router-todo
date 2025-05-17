a3504423f282bb1c4f775abd5a0cec4828d9b08a
もっちゃん <s7056i3056@gmail.com>
2025-05-17 17:32:25 +0900

    feat: editを追加

--- src/app/todo/[todoId]/edit/page.tsx
+++ src/app/todo/[todoId]/edit/page.tsx
@@ -1,9 +1,48 @@
-const TodoEdit = () => {
+import { redirect } from "next/navigation";
+import { PrismaClient } from "../../../../../generated/prisma";
+
+const prisma = new PrismaClient();
+
+const handleUpdate = async (formData: FormData) => {
+  "use server";
+
+  const todoId = formData.get("id");
+  const todo = formData.get("todo");
+
+  if (todoId === null || todo === null) {
+    return;
+  }
+
+  await prisma.todos.update({
+    where: {
+      id: parseInt(todoId.toString()),
+    },
+    data: {
+      todo: todo.toString(),
+    },
+  });
+
+  redirect("/todo");
+};
+
+const TodoEdit = async ({ params }: { params: { todoId: string } }) => {
+  const todo = await prisma.todos.findUnique({
+    where: {
+      id: Number(params.todoId),
+    },
+  });
+
+  if (!todo) {
+    console.warn(`todoId: ${params.todoId} not found`);
+    return redirect("/todo");
+  }
+
   return (
     <>
       <h2>todo edit</h2>
-      <form action="/todo/1/edit" method="post">
-        <input type="text" />
+      <form action={handleUpdate}>
+        <input type="hidden" name="id" value={todo.id} />
+        <input type="text" name="todo" defaultValue={todo.todo} />
       </form>
       <form action="/todo/1/delete" method="post">
         <button>delete</button>
