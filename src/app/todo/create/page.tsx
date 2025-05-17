9b105be4ade476ea2af88afed79b868b0170e2c2
もっちゃん <s7056i3056@gmail.com>
2025-05-17 17:09:04 +0900

    feat: create を実装

--- src/app/todo/create/page.tsx
+++ src/app/todo/create/page.tsx
@@ -1,9 +1,29 @@
+import { PrismaClient } from "../../../../generated/prisma";
+
+const prisma = new PrismaClient();
+
+const handleCreate = async (formData: FormData) => {
+  "use server";
+
+  const todo = formData.get("todo");
+
+  if (todo === null) {
+    return;
+  }
+
+  await prisma.todos.create({
+    data: {
+      todo: todo.toString(),
+    },
+  });
+};
+
 const TodoCreate = () => {
   return (
     <>
       <h2>todo create</h2>
-      <form action="/todo" method="post">
-        <input type="text" />
+      <form action={handleCreate}>
+        <input type="text" name="todo" />
       </form>
     </>
   );
